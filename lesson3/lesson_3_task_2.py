63dd71c97155001ab8aab1a7ce14d7a66c329b54
olgagol12 <olgagol12@mail.ru>
2024-07-27 06:54:28 +0300

    addthird homework

---
+++ lesson3/lesson_3_task_2.py
@@ -0,0 +1,11 @@
+from smartphone import Smartphone
+catalog =  ["gg",  " - ", "12", ". ", "+79809993344 " "\n",
+           "jj", " - ", "11",  ". ", "+79844667787 " "\n",
+           "jfjf", " - ", "15", ". ", "+79878980055 " "\n",
+           "sss", " - ", "1yhd", ". ", "+79909008786 " "\n",
+            "ddd", " - ", "jdhf",  ". ", "+79456789090 " "\n"]
+
+for x in range (0, 25):
+    print (catalog[x], end = "")
+
+
