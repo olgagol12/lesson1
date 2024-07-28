978bd001165281421a524a55ed9871f342d4b7d0
olgagol12 <161331018+olgagol12@users.noreply.github.com>
2024-07-28 15:32:39 +0300

    Update lesson_2_task_2.py

--- lesson2/lesson_2/lesson_2_task_2.py
+++ lesson2/lesson_2/lesson_2_task_2.py
@@ -1,6 +1,5 @@
 is_year_leap = int(input("Введите год:"))
 if (is_year_leap % 4 == 0):
-    print (True)
+    print ("год", is_year_leap,":", True)
 else:
-    print (False)
-
+    print ("год", is_year_leap,":", False)
